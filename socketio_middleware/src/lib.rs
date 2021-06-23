c0fd4890a5ec67882b219799ab5d4ef9bd949fd0
Gordon Wang <gordon.wang@zerologix.com>
2021-06-23 16:34:58 +1000

    restore rooms to pub

--- socketio_middleware/src/lib.rs
+++ socketio_middleware/src/lib.rs
@@ -7,7 +7,7 @@ extern crate log;
 pub use thruster_socketio_proc::*;
 
 pub mod redis_pubsub;
-mod rooms;
+pub mod rooms;
 mod sid;
 mod socketio;
 mod socketio_context;
@@ -18,4 +18,3 @@ mod socketio_upgrade;
 pub use socketio::{adapter, SocketIOAdapter, SocketIOSocket as SocketIO, broadcast};
 pub use socketio_context::SocketIOContext;
 pub use socketio_upgrade::handle_io;
-pub use rooms::get_sockets_for_room;
